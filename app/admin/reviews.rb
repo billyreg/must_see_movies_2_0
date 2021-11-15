0cb7331bb22215878cb0f321cc4ebdd7aea38c43
anonymous anonymous <bill.reggio@kellogg.northwestern.edu>
2021-11-15 21:27:31 +0000

    Whitelist columns in Review for ActiveAdmin

--- app/admin/reviews.rb
+++ app/admin/reviews.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Review do
 
+ permit_params :movie_id, :rating, :user_id, :comment
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
