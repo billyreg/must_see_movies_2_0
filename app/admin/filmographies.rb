2dd864f132c2f605b3a7328771c538fbd61488cd
anonymous anonymous <bill.reggio@kellogg.northwestern.edu>
2021-11-15 21:27:28 +0000

    Whitelist columns in Filmography for ActiveAdmin

--- app/admin/filmographies.rb
+++ app/admin/filmographies.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Filmography do
 
+ permit_params :movie_id, :director_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
